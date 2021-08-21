a2a116c8fcbeef4599180fcdb9c9f046b83385a0
Paolo Enrico Iacono Fullone <paolo.enrico@gmail.com>
2021-08-21 09:11:29 -0300

    Criado método cumprimentar.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):  #poderia ser qq dois, em java o self é this, no python sempre colocamos self
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__': # a main serve para fazermos os testes.
+    p = Pessoa() # criamos uma pessoa p
+    print(Pessoa.cumprimentar(p)) #agora vamos executar o método, Pessoa, pedimos o cumprimentar e passamos o pessoa.
+    print(id(p))
+    print(p.cumprimentar()) # Podemos executar o método a partir do objeto p e ter o mesmo resultado do 'print(Pessoa.cumprimentar(p))'
